4e41f54680f8f8be46640f2b5fabc0b056d1e5ac
cassidybrook <cassidybrookland2@gmail.com>
2023-03-07 09:44:46 +1300

    Add project files.

---
+++ Source and Version control/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Source_and_Version_control.MvcApplication" Language="C#" %>
---
+++ Source and Version control/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Source_and_Version_control.Startup))]
+namespace Source_and_Version_control
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
